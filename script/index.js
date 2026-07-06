ace0c23f3d1403d4f8374ba417fdce5fee90ed55
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-07-06 18:39:40 +0600

    before the api declear

--- script/index.js
+++ script/index.js
@@ -4,6 +4,30 @@ const loadlessons = () => {
    .then((json) => displaylesson(json.data))
 }
 
+const loadlevelword = (id) =>
+{
+   const url = `https://openapi.programming-hero.com/api/level/5`
+   
+   fetch(url)
+   .then((res) => res.json())
+   .then((data) => displevelworld(data.data));
+};
+
+const displevelworld = (words) => {
+   
+   const wordcontainer = document.getElementById("word-container");
+   // wordcontainer.innerHTML ="";
+
+   words.forEach((word) => {
+      console.log(word);
+      const card = document.createElement("div");
+      card.innerHTML = `
+      <p>cat</p> 
+      `;
+      wordcontainer.append(card);
+   });
+}
+
 const displaylesson = (lessons) => {
 
    const levelcontiner = document.getElementById("level-container")
@@ -13,7 +37,7 @@ const displaylesson = (lessons) => {
       console.log(lesson);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-     <button class="btn btn-outline btn-primary">
+     <button onclick = "loadlevelword(${lesson.level_no})" class="btn btn-outline btn-primary">
         
           <i class="fa-solid fa-book-open"></i>
           Lessson - ${lesson.level_no}
